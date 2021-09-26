43820e947022c522bacc84400fb426ae109e61a3
Afnan <afnan.qureshi.x1@gmail.com>
2021-09-27 00:33:16 +0500

    accessing blockchain through an api part two. added post and get methods

--- dev/api.js
+++ dev/api.js
@@ -2,10 +2,19 @@
 const express = require('express');
 const app = express();
  
-app.get('/', function (req, res) {
-  res.send('Hello World Of blockchain');
+app.get('/blockchainDemo', function (req, res) {
+  res.send('Hello World Of blockchain of believers');
 })
  
+app.get('/blockchain', function (req, res) {
+  res.send('Here we will show our complete blockchain');
+})
+
+app.post('/transaction', function (req, res) {
+  res.send('Here we will call our create new transaction method from our blockchain dataStructure');
+
+})
+
 app.listen(3000, function(){
   console.log("this server runs on local port 3000");
 });
