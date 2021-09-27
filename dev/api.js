c5c8a3831a3b687125d7b75724f18733020e42ed
Afnan <afnan.qureshi.x1@gmail.com>
2021-09-27 13:10:17 +0500

    added 2 end points. i.e post transaction and get blockchain through postman api

--- dev/api.js
+++ dev/api.js
@@ -1,19 +1,28 @@
 
 const express = require('express');
 const app = express();
- 
+const Blockchain = require('./blockchain');
+const bitcoin = new Blockchain();
+const bodyParser = require ('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+
 app.get('/blockchainDemo', function (req, res) {
   res.send('Hello World Of blockchain of believers');
 })
  
 app.get('/blockchain', function (req, res) {
-  res.send('Here we will show our complete blockchain');
+  res.send(bitcoin);
 })
 
 app.post('/transaction', function (req, res) {
-  res.send('Here we will call our create new transaction method from our blockchain dataStructure');
+  const blockIndex =  bitcoin.createNewTransaction(req.body.amount , req.body.sender , req.body.recipient);
+  res.json({note : `this transaction will be saved in block ${blockIndex}`});
+  
 
-})
+});
 
 app.listen(3000, function(){
   console.log("this server runs on local port 3000");
