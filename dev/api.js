79fb6d3e8c7fa916f9751cd244a54d9c709d0268
Afnan <afnan.qureshi.x1@gmail.com>
2021-10-04 16:36:46 +0500

    accessing api part 4 i..e created mine end point for creating new block

--- dev/api.js
+++ dev/api.js
@@ -2,8 +2,12 @@
 const express = require('express');
 const app = express();
 const Blockchain = require('./blockchain');
-const bitcoin = new Blockchain();
 const bodyParser = require ('body-parser');
+const  {v4 :  uuidv4}  = require('uuid'); // it will generate random string for us.. which we will use for uniquely identifying miner address for now 
+
+const nodeAddress = uuidv4().split('-').join(''); // to cut off the dashes(-) that uuid in general puts between strings just to be clear with strings
+
+const bitcoin = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
@@ -12,16 +16,40 @@ app.use(bodyParser.urlencoded({extended : false}));
 app.get('/blockchainDemo', function (req, res) {
   res.send('Hello World Of blockchain of believers');
 })
- 
+ // this will give us a complete blockchain
 app.get('/blockchain', function (req, res) {
   res.send(bitcoin);
 })
-
+//this is for creating a new transaction 
 app.post('/transaction', function (req, res) {
   const blockIndex =  bitcoin.createNewTransaction(req.body.amount , req.body.sender , req.body.recipient);
-  res.json({note : `this transaction will be saved in block ${blockIndex}`});
-  
+  res.json({note : `this transaction will be saved in block ${blockIndex}`});  
+});
 
+//mine a NEw Block
+app.get('/mine',function(req, res){
+ 
+  const lastBlock = bitcoin.getLastBlock(); 
+  const previosBlockHash = lastBlock['hash']; // to get previous block hash taking one property 
+
+  const currentBlockData = {
+    transaction : bitcoin.pendingTransactions,
+    index : lastBlock['index'] +1 ,
+  }
+
+  const nonce = bitcoin.proofOfWork(previosBlockHash, currentBlockData);
+
+  const blockHash = bitcoin.hashBlock(previosBlockHash, currentBlockData, nonce); 
+  //transaction for miner reward
+  bitcoin.createNewTransaction(6.5 , "000000000" , nodeAddress ) //nodeaddress is the miner address "MinerADRESSSSSSSShouldBeUnique so use uuid
+  
+  const newBlock = bitcoin.CreateNewBlock(nonce, previosBlockHash , blockHash);
+
+  res.json({
+    note : "New Block Mined Successfully ! ! !",
+    block : newBlock,
+
+  });
 });
 
 app.listen(3000, function(){
